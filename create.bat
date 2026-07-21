2328111d2692231e2d837cd1550dc73d63b0f3ca
lpxsbr <anselmolopes.an@gmail.com>
2026-07-21 14:11:42 -0300

    teste 1

---
+++ create.bat
@@ -0,0 +1,14 @@
+@echo off
+
+set BASE=develop
+
+git checkout %BASE%
+
+for %%B in (a b c d e f g h i j) do (
+    git checkout -b dev-%%B
+    git push -u origin dev-%%B
+    git checkout %BASE%
+)
+
+echo Concluido!
+pause
